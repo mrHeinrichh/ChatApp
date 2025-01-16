11cf744d191a791b3f1e79bb57a817dd2bad4784
Heinric <johnheinrich.fabros@tup.edu.ph>
2025-01-16 16:42:01 +0800

    update 1 file and create 1 file

--- lib/CustomUI/CustomCard.dart
+++ lib/CustomUI/CustomCard.dart
@@ -5,22 +5,41 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      leading: CircleAvatar(
-        radius: 25,
-      ),
-      title: Text(
-        'Title',
-        style: TextStyle(fontSize: 16),
-      ),
-      subtitle: Row(
+    return GestureDetector(
+      onTap: () {},
+      child: const Column(
         children: [
-          Icon(Icons.done_all),
-          SizedBox(width: 3),
-          Text('Subtitle'),
+          ListTile(
+            leading: CircleAvatar(
+              radius: 25,
+              child: Icon(
+                Icons.group,
+                size: 38,
+              ),
+              backgroundColor: Colors.blueGrey,
+            ),
+            title: Text(
+              'Title',
+              style: TextStyle(fontSize: 16),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(Icons.done_all),
+                SizedBox(width: 3),
+                Text('Subtitle'),
+              ],
+            ),
+            trailing: Text('18:04'),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 20, left: 80),
+            child: Divider(
+              thickness: 0.5,
+              color: Colors.blueGrey,
+            ),
+          ),
         ],
       ),
-      trailing: Text('18:04'),
     );
   }
 }
---
+++ lib/Model/ChatModel.dart
@@ -0,0 +1,14 @@
+class ChatModel {
+  final String name;
+  final String icon;
+  bool isGroup;
+  final String time;
+  final String currentMessage;
+
+  ChatModel(
+      {required this.name,
+      required this.icon,
+      required this.isGroup,
+      required this.time,
+      required this.currentMessage});
+}
