e084fc9723f1288a21f695db59113148ef081558
Heinrich Fabros <97886828+mrHeinrichh@users.noreply.github.com>
2025-01-25 23:19:50 +0800

    create 1 file and update 3 files

--- lib/Screens/IndividualPage.dart
+++ lib/Screens/IndividualPage.dart
@@ -2,6 +2,7 @@ import 'package:chatapp/CustomUI/OwnMessageCard.dart';
 import 'package:chatapp/CustomUI/ReplyCard.dart';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class IndividualPage extends StatefulWidget {
   IndividualPage({Key? key, this.chatModel}) : super(key: key);
@@ -12,10 +13,14 @@ class IndividualPage extends StatefulWidget {
 }
 
 class _IndividualPageState extends State<IndividualPage> {
+  IO.Socket? socket;
   bool showEmojiPicker = false;
   TextEditingController _controller = TextEditingController();
+  String socketStatus = "Disconnected";
+  @override
   void initState() {
     super.initState();
+    connect();
     widget.focusNode.addListener(() {
       if (widget.focusNode.hasFocus) {
         setState(() {
@@ -24,6 +29,37 @@ class _IndividualPageState extends State<IndividualPage> {
       }
     });
   }
+
+ void connect() {
+  socket = IO.io("http://192.168.0.110:5000", <String, dynamic>{
+    "transports": ["websocket"],
+    "autoConnect": false,
+  });
+
+  socket!.onConnect((data) {
+    print("Connected");
+    setState(() {
+      socketStatus = "Connected";
+    });
+    socket!.emit("message", "Hello fromasdasdas Flutter");
+  });
+
+  socket!.onConnectError((error) {
+    print("Connection error: $error");
+    setState(() {
+      socketStatus = "Connection error: $error";
+    });
+  });
+
+  socket!.onDisconnect((_) {
+    print("Disconnected");
+    setState(() {
+      socketStatus = "Disconnected";
+    });
+  });
+
+  socket!.connect();
+}
 
   @override
   Widget build(BuildContext context) {
@@ -41,22 +77,22 @@ class _IndividualPageState extends State<IndividualPage> {
                       Navigator.pop(context);
                     },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: CircleAvatar(
-                      radius: 20,
-                      child: widget.chatModel.isGroup
-                          ? const Icon(
-                              Icons.group,
-                              size: 38,
-                            )
-                          : const Icon(
-                              Icons.person,
-                              size: 38,
-                            ),
-                      backgroundColor: Colors.blueGrey,
-                    ),
-                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(left: 8.0),
+                  //   child: CircleAvatar(
+                  //     radius: 20,
+                  //     child: widget.chatModel.isGroup
+                  //         ? const Icon(
+                  //             Icons.group,
+                  //             size: 38,
+                  //           )
+                  //         : const Icon(
+                  //             Icons.person,
+                  //             size: 38,
+                  //           ),
+                  //     backgroundColor: Colors.blueGrey,
+                  //   ),
+                  // ),
                 ],
               ),
             ),
@@ -71,7 +107,7 @@ class _IndividualPageState extends State<IndividualPage> {
                       Text(widget.chatModel.name),
                       const Text(
                         'Last seen today at 12:05',
-                        style: TextStyle(fontSize: 13),
+                        style: TextStyle(fontSize: 9),
                       ),
                     ],
                   )),
@@ -104,11 +140,11 @@ class _IndividualPageState extends State<IndividualPage> {
             child: WillPopScope(
               child: Stack(
                 children: [
-                  Container(
+                  SizedBox(
                     height: MediaQuery.of(context).size.height - 170,
                     child: ListView(
                       shrinkWrap: true,
-                      children: [
+                      children: const [
                         OwnMessageCard(),
                         ReplyCard(),
                         OwnMessageCard(),
